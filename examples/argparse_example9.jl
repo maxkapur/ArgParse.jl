82d2bdc5e41d1176c9552f961d77711b5da5dbcf
Max Kapur <maxkapur@gmail.com>
2024-06-23 14:08:40 -0400

    Make example more complicated

--- examples/argparse_example9.jl
+++ examples/argparse_example9.jl
@@ -4,22 +4,35 @@ using ArgParse
 using Markdown
 
 function main(args)
-
     # initialize the settings (the description is for the help screen)
-    s = ArgParseSettings(description = md"Example 9 for `argparse.jl`: Markdown strings.")
+    s = ArgParseSettings(
+        description=md"""
+        Example 9 for `argparse.jl`: Markdown strings. This description text
+        uses various Markdown features such as **strong text**, text with
+        *emphasis*, `code snippets`, and math: ``y = mx + b``.
+
+        The following list should be numbered automatically:
+
+        1. Here
+        1. Is
+        1. A
+        1. List
+        """,
+        epilog=md"See [our website](https://example.com) for more information."
+    )
 
     @add_arg_table! s begin
         "--opt1"               # an option (will take an argument)
         "--opt2", "-o"         # another option, with short form
         "src"                  # a positional argument
-           help = md"The **source** of all our troubles"
+            help = md"The **source** of all our troubles"
         "dest"                 # a positional argument
-           help = md"The **destination** where we want to send them"
+            help = md"The **destination** where we want to send them"
     end
 
     parsed_args = parse_args(s) # the result is a Dict{String,Any}
     println("Parsed args:")
-    for (key,val) in parsed_args
+    for (key, val) in parsed_args
         println("  $key  =>  $(repr(val))")
     end
 end
