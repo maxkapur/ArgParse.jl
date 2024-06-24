c3a21c815ce30dd6c80f7a4931d4ab7bba7abf87
Max Kapur <maxkapur@gmail.com>
2024-06-24 17:26:49 -0400

    Update test

--- test/argparse_test15.jl
+++ test/argparse_test15.jl
@@ -40,7 +40,7 @@ end
 let s = ap_settings15()
     ap_test15(args) = parse_args(args, s)
 
-    @test stringhelp(s) == "usage: argparse_test15.jl [--opt1 OPT1] [--opt2 OPT2]\n\nTest 15 for \e[36margparse.jl\e[39m: Markdown strings. This description text uses\n  various Markdown features such as \e[1mstrong text\e[22m, text with \e[4memphasis\e[24m, \e[36mcode\n  snippets\e[39m, and math: \e[35my = mx + b\e[39m.\n\n\e[1m  Subheading\e[22m\n\e[1m  ==========\e[22m\n\n  The following list should be numbered automatically:\n\n    1. Here\n\n    2. Is\n\n    3. A\n\n    4. List\n\noptional arguments:\n  --opt1 OPT1  a flag with String helptext (type: Int64)\n  --opt2 OPT2  a flag with ~~String~~ \e[1mmarkdown\e[22m helptext\n\nSee our website (https://example.com) for more information.\n\n"
+    @test stringhelp(s) == "usage: argparse_test15.jl [--opt1 OPT1] [--opt2 OPT2]\n\nTest 15 for \e[36margparse.jl\e[39m: Markdown strings. This description text\n  uses various Markdown features such as \e[1mstrong text\e[22m, text with\n  \e[4memphasis\e[24m, \e[36mcode snippets\e[39m, and math: \e[35my = mx + b\e[39m.\n\n\e[1m  Subheading\e[22m\n\e[1m  ==========\e[22m\n\n  The following list should be numbered automatically:\n\n    1. Here\n\n    2. Is\n\n    3. A\n\n    4. List\n\noptional arguments:\n  --opt1 OPT1  a flag with String helptext (type: Int64)\n  --opt2 OPT2  a flag with ~~String~~ \e[1mmarkdown\e[22m helptext\n\nSee our website (https://example.com) for more information.\n\n"
 
     @test ap_test15([]) == Dict{String,Any}("opt1"=>nothing, "opt2"=>nothing)
     @test ap_test15(["--opt1", "33"]) == Dict{String,Any}("opt1"=>33, "opt2"=>nothing)
